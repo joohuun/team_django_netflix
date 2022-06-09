ff55f674e7613ce7bb0d1fdd512e05d44f978d69
DESKTOP-V6HUDEC\user <dbd02017@gmail.com>
2022-06-09 10:42:03 +0900

    init

---
+++ movie/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views 
+# 지금 내가 있는 폴더에서 views라는 파이썬 파일을 가져올거야!
+
+urlpatterns = [
+    #path('', views.csvTomodel, name='csvTomodel'),
+    path('', views.home, name='home'),
+    
+]
---
+++ movie/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+import re
+from urllib import request
+import csv
+
+from .models import *
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+@login_required
+def home(request):
+    if request.method == 'GET':
+        movies = MovieModel.objects.all().order_by('title')             
+        return render(request, 'movie/home.html', {'movies':movies})
