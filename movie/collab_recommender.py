63ad25d5eb69f0a9be0c1a86d979bba5b7061c2b
wonbbnote <wonbb3313@gmail.com>
2022-06-13 10:41:38 +0900

    3차 머지 -  로그인, 회원가입 html 머지중

--- movie/collab_recommender.py
+++ movie/collab_recommender.py
@@ -1,23 +1,26 @@
 # from .models import *
-# import pandas as pd
-# import pymysql.cursors
+import pandas as pd
+import pymysql.cursors
 
-# connection = pymysql.connect(
-#     host='localhost',
-#     port = 3306,
-#     user='root',
-#     password='#won971219',
-#     db='django-netflix',
-#     charset = 'utf8',
-#     autocommit = True,
-#     cursorclass=pymysql.cursors.DictCursor
-# )
-# cursor = connection.cursor()
-# sql = "select * from "
-# cursor.execute(sql)
-# result = cursor.fetchall()
-# print(result)
+connection = pymysql.connect(
+    host='localhost',
+    port = 3306,
+    user='root',
+    password='#won971219',
+    db='django_netflix',
+    charset = 'utf8',
+    autocommit = True,
+    cursorclass=pymysql.cursors.DictCursor
+)
+cursor = connection.cursor()
+sql = "select * from comment;"
+cursor.execute(sql)
+result = cursor.fetchall()
+df = pd.DataFrame(result)
 
+# ratings_matrix = ratings.pivot_table("rate", "userid", "movieid")
+
+print(df)
 # def collab_recommender():
 #     comment_data = MovieComment.objects.all()
 #     comment = pd.DataFrame(comment_data)
--- team_django_netflix/settings.py
+++ team_django_netflix/settings.py
@@ -126,6 +126,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.UserModel'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+STATICFILES_DIRS = [ BASE_DIR/'static' ]
+
+STATIC_ROOT = BASE_DIR/'static_root' 
+MEDIA_ROOT = BASE_DIR/'media' 
+MEDIA_URL = '/media/' 
